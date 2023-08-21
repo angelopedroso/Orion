91d5f3956acf46c02101aa62b12df0639a327c32
angelopedroso <solonasusonly0@gmail.com>
2023-08-21 20:14:25 -0300

    feat: added shadcn ui

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -2,10 +2,21 @@
 
 import { Player } from '@lottiefiles/react-lottie-player'
 import React from 'react'
+import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar'
+import { DefaultUserIcon } from '../userIcon'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '../ui/dropdown-menu'
+import { Button } from '../ui/button'
 
 export function Header() {
   return (
-    <section className="flex items-center justify-between  px-20 py-8">
+    <section className="flex items-center justify-between pb-8">
       <div className="flex items-center">
         <Player
           className="h-16 w-16 bg-transparent"
@@ -15,6 +26,37 @@ export function Header() {
           autoplay
         />
         <h1 className="text-3xl font-bold text-slate-300">rion</h1>
+      </div>
+      <div className="flex items-center gap-3">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button
+              variant="ghost"
+              className="group h-10 w-10 p-0 hover:bg-transparent focus-visible:ring-offset-0"
+            >
+              <span className="sr-only">Open menu</span>
+              <Avatar className="h-full w-full hover:bg-transparent">
+                <AvatarImage
+                  src="https://github.com/angelopedroso.png"
+                  className="transition-all duration-300 hover:blur-sm"
+                />
+                <AvatarFallback className="bg-gray-500 transition-colors duration-300 hover:bg-violet-500">
+                  <DefaultUserIcon />
+                </AvatarFallback>
+              </Avatar>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent
+            side="bottom"
+            className="border-surface-primary bg-surface-primary-dark text-slate-200"
+          >
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator className="bg-surface-primary" />
+            <DropdownMenuItem className="cursor-pointer focus:bg-surface-primary focus:text-current">
+              Profile
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </section>
   )
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,61 @@
-import type { Config } from 'tailwindcss'
+import { Config } from 'tailwindcss'
 
 const config: Config = {
+  darkMode: ['class'],
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
   ],
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
     extend: {
+      colors: {
+        'surface-primary-dark': '#0d121d',
+        'surface-primary': '#111827',
+      },
       backgroundColor: {
         background: '#030712',
       },
+      width: {
+        536: '134rem',
+      },
+      height: {
+        536: '134rem',
+      },
+      rotate: {
+        '60': '60deg',
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+        floating: {
+          '0%': { transform: 'translateY(0px)' },
+          '50%': { transform: 'translateY(-30px)' },
+          '100%': { transform: 'translateY(0px)' },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+        floating: 'floating 6s ease-in-out infinite',
+      },
     },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-animate')],
 }
+
 export default config
