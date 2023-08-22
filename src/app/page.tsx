c9eb8f36ee12c68f1e24a2527c40301a121074ea
angelopedroso <solonasusonly0@gmail.com>
2023-08-22 13:05:30 -0300

    fix: responsivity from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,10 +12,10 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between lg:flex-row">
       <div className="flex h-full flex-col gap-4">
-        <h2 className="text-4xl font-bold text-slate-100 md:max-w-2xl md:text-7xl">
+        <h2 className="text-4xl font-bold text-slate-100 md:max-w-4xl md:text-7xl">
           Desvende o Inglês Autêntico
         </h2>
-        <div className="max-w-2xl">
+        <div className="max-w-6xl">
           <p className="text-md font-semibold text-slate-300">
             A ferramenta perfeita para auxiliar você em sua jornada de estudos
             utilizando o método{' '}
@@ -63,7 +63,7 @@ export default function Home() {
           Comece sua Jornada <MoveRight />
         </Link>
       </div>
-      <div className="flex h-full items-center justify-end">
+      <div className="flex h-80 w-80 items-center justify-end md:h-full md:w-full md:pt-2">
         <Image
           src={StudentImage}
           alt="Student"
