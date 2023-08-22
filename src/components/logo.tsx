9f15defe758b1243cb9b0e67bf0f7df6930a989f
angelopedroso <solonasusonly0@gmail.com>
2023-08-22 13:03:43 -0300

    feat: created logo component

---
+++ src/components/logo.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import React from 'react'
+
+import { Player } from '@lottiefiles/react-lottie-player'
+
+export function Logo() {
+  return (
+    <div className="flex items-center">
+      <Player
+        className="h-16 w-16 bg-transparent"
+        src="https://lottie.host/3b8e03bb-ddb5-4ca0-a69f-e5231351c4ad/vLFiMtpiKC.json"
+        speed={1}
+        loop
+        autoplay
+      />
+      <h1 className="-ml-2 text-3xl font-bold text-slate-300">rion</h1>
+    </div>
+  )
+}
