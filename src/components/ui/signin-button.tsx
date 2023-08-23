f9f17bc4b57376af5cb97edb92ba3ea839d27e25
angelopedroso <solonasusonly0@gmail.com>
2023-08-23 12:54:05 -0300

    feat: created handleSignIn function

--- src/components/ui/signin-button.tsx
+++ src/components/ui/signin-button.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { ReactNode } from 'react'
+import React, { ReactNode, useState } from 'react'
 import { Button } from './button'
 import { signIn } from 'next-auth/react'
 
@@ -10,12 +10,22 @@ export interface SignInButtonProps {
 }
 
 export function SignInButton({ children, provider }: SignInButtonProps) {
+  const [loading, setLoading] = useState(false)
   const isGithub = provider === 'github'
+
+  async function handleSignIn() {
+    setLoading(true)
+
+    const res = await signIn(provider, { callbackUrl: '/login/setting-up' })
+
+    setLoading(false)
+  }
 
   return (
     <Button
       className="text-md gap-4 bg-slate-100/90 py-7 font-semibold text-slate-950 hover:bg-slate-100/80"
-      onClick={() => signIn(provider)}
+      onClick={handleSignIn}
+      disabled={loading}
     >
       {children}
       Continue with {isGithub ? 'Github' : 'Google'}
