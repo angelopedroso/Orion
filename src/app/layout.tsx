be2428629d55cd14128d27f8a987af56d9d7c219
angelopedroso <solonasusonly0@gmail.com>
2023-08-24 20:40:45 -0300

    feat: added toaster

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,13 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { SessionCtxProvider } from '@/contexts/sessionContext'
+import { Toaster } from '@/components/ui/toaster'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Orion',
-  description: 'An english learning tool',
+  description: 'Uma ferramenta para auxiliar na aprendizagem do inglês.',
 }
 
 export default function RootLayout({
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} bg-background`}>
         <SessionCtxProvider>{children}</SessionCtxProvider>
+        <Toaster />
       </body>
     </html>
   )
