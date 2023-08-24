305d5beb6c6059473833915b903bfc587aece9b5
angelopedroso <solonasusonly0@gmail.com>
2023-08-24 18:50:46 -0300

    feat: added alert dialog on to try to sign out

--- src/components/Header/profile.tsx
+++ src/components/Header/profile.tsx
@@ -8,11 +8,22 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '../ui/dropdown-menu'
-import { Button } from '../ui/button'
+import { Button, buttonVariants } from '../ui/button'
 import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar'
 import { DefaultUserIcon } from '../../assets/userIcon'
 import { signOut, useSession } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '../ui/alert-dialog'
+import { LogIn, LogOut } from 'lucide-react'
 
 export function ProfileComponent() {
   const route = useRouter()
@@ -24,7 +35,7 @@ export function ProfileComponent() {
         <DropdownMenuTrigger asChild>
           <Button
             variant="ghost"
-            className="group h-10 w-10 p-0 hover:bg-transparent focus-visible:ring-offset-0"
+            className="h-10 w-10 p-0 hover:bg-transparent focus-visible:ring-offset-0"
           >
             <span className="sr-only">Open menu</span>
             <Avatar className="h-full w-full hover:bg-transparent">
@@ -43,20 +54,47 @@ export function ProfileComponent() {
           className="border-surface-primary bg-surface-primary-dark text-slate-200"
           align="end"
         >
-          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuLabel>Meu Perfil</DropdownMenuLabel>
           <DropdownMenuSeparator className="bg-surface-primary" />
           <DropdownMenuItem
             className="cursor-pointer focus:bg-surface-primary focus:text-current"
             onClick={() => route.push('/login/setting-up')}
           >
-            Profile
+            Perfil
           </DropdownMenuItem>
-          <DropdownMenuItem
-            className="cursor-pointer focus:bg-surface-primary focus:text-current"
-            onClick={() => signOut({ callbackUrl: '/' })}
-          >
-            Sign out
-          </DropdownMenuItem>
+
+          <AlertDialog>
+            <AlertDialogTrigger asChild>
+              <Button
+                variant={'ghost'}
+                className="h-fit w-full justify-start rounded-sm px-2 text-sm font-normal text-white hover:bg-surface-primary hover:text-current"
+              >
+                Sair
+              </Button>
+            </AlertDialogTrigger>
+            <AlertDialogContent className="py-14">
+              <AlertDialogHeader>
+                <AlertDialogTitle>
+                  Tem certeza que quer sair? 😥
+                </AlertDialogTitle>
+              </AlertDialogHeader>
+              <AlertDialogFooter className="gap-8">
+                <AlertDialogCancel className="flex h-full w-36 flex-col gap-2 border-0 bg-violet-500 px-8 py-6 text-white hover:bg-violet-400 hover:text-white">
+                  <LogIn className="h-14 w-14" />
+                  Outra hora
+                </AlertDialogCancel>
+                <AlertDialogAction
+                  onClick={() => signOut({ callbackUrl: '/' })}
+                  className={`${buttonVariants({
+                    variant: 'secondary',
+                  })}flex h-full w-36 flex-col gap-2 px-8 py-6`}
+                >
+                  <LogOut className="h-14 w-14" />
+                  Sair
+                </AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
         </DropdownMenuContent>
       </DropdownMenu>
     </div>
