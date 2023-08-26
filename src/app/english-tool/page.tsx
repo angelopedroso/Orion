7d218daa71d45bdbfb688ce7a8304d154d38943a
angelopedroso <solonasusonly0@gmail.com>
2023-08-26 00:04:35 -0300

    feat: created EnglishTool page

--- src/app/english-tool/page.tsx
+++ src/app/english-tool/page.tsx
@@ -1,7 +1,24 @@
+import { EnglishForm } from '@/components/english-tool-form'
+import { EnglishResponse } from '@/components/english-tool-response'
+import { getCurrentUser } from '@/lib/session'
+import { getDecryptedToken } from '@/utils/getDecryptedToken'
 import React from 'react'
 
-export interface EnglishToolProps {}
+export default async function EnglishTool() {
+  const session = await getCurrentUser()
+  const token = await getDecryptedToken(session?.id)
 
-export default function EnglishTool(props: EnglishToolProps) {
-  return <h1 className="text-white">ENGLISH</h1>
+  return (
+    <main className="flex flex-1 justify-center px-2 pb-8">
+      <section className="flex w-full flex-col space-y-8 rounded-lg text-white shadow-md md:px-10 md:py-6 md:nm-convex-background-900-xl lg:w-3/5">
+        {/* Form */}
+        <div className="flex w-full justify-center">
+          <EnglishForm promptKey={token!} />
+        </div>
+
+        {/* Response */}
+        <EnglishResponse />
+      </section>
+    </main>
+  )
 }
