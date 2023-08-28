35303e7490f6054c48e837ed25435b37ef9733ba
angelopedroso <solonasusonly0@gmail.com>
2023-08-28 16:23:37 -0300

    fix: updated prompt

--- src/app/api/completion/route.ts
+++ src/app/api/completion/route.ts
@@ -40,12 +40,11 @@ export async function POST(req: Request) {
   
      Dizer o significado dela em português, falar se pode ser usada como verbo, substantivo... Dizer palavras equivalentes em português (se houver mais de uma dizer no máximo 5 palavras equivalentes), e como se pronuncia a palavra em inglês: por exemplo: /wāk/;
      Também deve fornecer a semelhança da pronuncia em português: uaiki;
-     Deve dizer todos os significados possíveis, por exemplo, spring pode ser primavera e mola, além de poder ser o verbo pular;
+     Deve dizer TODOS os significados possíveis, por exemplo, spring pode ser primavera e mola, além de poder ser o verbo pular;
      A explicação do significado não precisa ser muito extensa;
-     Quando a palavra for em algum tempo verbal diferente do presente, toda a resposta deve ser retornada com a palavra recebida no presente. Por exemplo: "Gave up" para "Give up". A pronuncia em inglês também deve ser referente a palavra no presente;
-     O retorno deve ser em formato JSON. Sendo as keys em ingles, exemplo: {"Word": "Hug"...};
      Os meaning deve ser em formato de array de objetos contendo um campo "type" que é responsavel por dizer o tipo de palavra (Verbo, Substantivo...), além da explicação;
      Provide 10 english example's phrase with it word, the phrases must be simple, short and without translation to portuguese, just english;
+     VOCÊ SÓ EXPLICA PALAVRAS E EXPRESSÕES;
      JSON must be returned to the following forme:
      {
        "word": "",
@@ -62,7 +61,7 @@ export async function POST(req: Request) {
        "exampleSentences": [""]
      }
   
-     SE A PALAVRA NÃO EXISTIR ME RETORNE O SEGUINTE JSON:
+     SE A PALAVRA NÃO EXISTIR OU SE O USUÁRIO DIGITAR UMA FRASE QUE NÃO SEJA UMA EXPRESSÃO ME RETORNE O SEGUINTE JSON:
   
      {
       "word": null,
@@ -71,6 +70,7 @@ export async function POST(req: Request) {
      A partir da explicação acima, escreva o JSON a partir da palavra abaixo:
      Forneça EXATAMENTE 10 FRASES;
      Word, useType e Type devem ser SEMPRE a PRIMEIRA LETRA em maiuscula;
+
   
      Palavra: ${prompt}
     `
